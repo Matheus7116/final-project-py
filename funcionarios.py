4b3026eb34b50e6d28c9afd61622bae8ab184ee9
Matheus Colares <matheusarraiscolares@hotmail.com>
2026-03-31 15:17:25 -0300

    primeiro commit

---
+++ funcionarios.py
@@ -0,0 +1,81 @@
+def menu_funcionarios(conexao):
+    while True:
+        print("\n=== FUNCIONÁRIOS ===")
+        print("1 - Cadastrar funcionário")
+        print("2 - Ver todos os funcionários")
+        print("3 - Editar funcionário")
+        print("4 - Excluir funcionário")
+        print("0 - Voltar")
+
+        opcao = input("Escolha: ")
+
+        if opcao == "1":
+            cadastrar_funcionario(conexao)
+        elif opcao == "2":
+            verfuncionarios(conexao)
+        elif opcao == "3":
+            editarfuncionario(conexao)
+        elif opcao == "4":
+            excluirfuncionario(conexao)
+        elif opcao == "0":
+            print("Voltando...")
+            break
+        else:
+            print("Opção inválida!")
+
+def cadastrar_funcionario(conexao):
+    cursor = conexao.cursor(dictionary=True)
+    nome = input("Nome: ")
+    cargo = input("Cargo: ")
+    salario = input("Salário: ")
+    cursor.execute(f"""
+        INSERT INTO funcionarios (nome, cargo, salario)
+        VALUES ('{nome}', '{cargo}', '{salario}')
+    """)
+    conexao.commit()
+    cursor.close()
+    print(f"Funcionário {nome} cadastrado com sucesso")
+
+def verfuncionarios(conexao):
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM funcionarios')
+    for element in cursor.fetchall():
+        print(f"""
+        INFORMAÇÕES DO FUNCIONÁRIO {element['id']}
+        Nome: {element['nome']}
+        Cargo: {element['cargo']}
+        Salário: {element['salario']}
+    """)
+    cursor.close()
+
+def excluirfuncionario(conexao):
+    cursor = conexao.cursor(dictionary=True)
+    id = int(input("Digite o id do funcionário que deseja excluir: "))
+    cursor.execute(f"DELETE FROM funcionarios WHERE id = {id}")
+    conexao.commit()
+    cursor.close()
+    print("Funcionário excluído com sucesso")
+
+def editarfuncionario(conexao):
+    cursor = conexao.cursor(dictionary=True)
+    id = int(input("Digite o id do funcionário que deseja editar: "))
+    
+    cursor.execute(f"SELECT * FROM funcionarios WHERE id = {id}")
+    funcionario = cursor.fetchone()
+    
+    if funcionario:
+        novo_nome = input(f"Nome ({funcionario['nome']}): ") 
+        novo_cargo = input(f"Cargo ({funcionario['cargo']}): ") 
+        novo_salario = input(f"Salário ({funcionario['salario']}): ") 
+
+        cursor.execute(f"""
+            UPDATE funcionarios
+            SET nome = '{novo_nome}', cargo = '{novo_cargo}', salario = '{novo_salario}'
+            WHERE id = {id}
+        """)
+        conexao.commit()
+        print("Funcionário atualizado com sucesso")
+    else:
+        print("Funcionário não encontrado")
+
+    cursor.close()
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import mysql.connector
 from produtos import menu_produtos 
+from funcionarios import menu_funcionarios
 
 conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='hercules123.Batata',
-    database='SUPERMERCADO',
+    host='DB_HOST',
+    user='DB_USER',
+    password='DB_PASSWORD',
+    database='DB_NAME',
 )
 
 def menu():
     while True:
         print("\n=== SUPERMERCADO ===")
         print("1 - Ver produtos")
-        print("2 - Ver clientes")
-        print("3 - Ver funcionários")
+        print("2 - Ver funcionários")
+        print("3 - Ver clientes")
         print("4 - Ver vendas")
         print("0 - Sair")
 
@@ -21,6 +22,12 @@ def menu():
 
         if opcao == "1":
             menu_produtos(conexao) 
+        if opcao == "2":
+            print("Em construção...")
+        if opcao == "3":
+            print("Em construção...")
+        if opcao == "4":
+            print("Em construção...")
         elif opcao == "0":
             print("Saindo...")
             break
